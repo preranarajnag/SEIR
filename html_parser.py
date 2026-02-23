4aca8c566372778e88217b24525c4d13c6ec9478
preranarajnag <preranarajnag2809@gmail.com>
2026-02-23 23:26:43 +0530

    Add files via upload

---
+++ html_parser.py
@@ -0,0 +1,93 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():    
+    if len(sys.argv) == 2:
+        url = sys.argv[1]
+    else:
+        print("Enter in the correct format!")
+        print("file_name.py  URL")
+        return
+
+    try:
+        sendRequest = requests.get(url)
+
+        if sendRequest.status_code == 200:
+            print("Successfully page is fetched")
+
+            HTMLcontent = BeautifulSoup(sendRequest.text, "html.parser")
+
+            if HTMLcontent.title is not None and HTMLcontent.title.string is not None:
+                print(HTMLcontent.title.string.strip())
+            else:
+                print("No Titles in the page")
+
+            bdytxt = HTMLcontent.get_text(separator=" ", strip=True)
+
+            if bdytxt:
+                print(bdytxt)
+            else:
+                print("No Body Content in the page")
+                return
+
+            lks = HTMLcontent.find_all("a")
+
+            if len(lks) > 0:
+                for link in lks:
+                    if link.get("href") is not None:
+                        print(link.get("href"))
+            else:
+                print("No Links Found in the page")
+
+            # Counting the frequency
+            bdytxt = bdytxt.lower()
+
+            wordslist = []
+            prword = ""
+
+            for i in bdytxt:
+                if i.isalnum():
+                    prword += i
+                else:
+                    if prword != "":
+                        wordslist.append(prword)
+                        prword = ""
+
+            if prword != "":
+                wordslist.append(prword)
+
+            # Create empty dictionary -> Counting frequency
+            word_freq = {}
+
+            for i in wordslist:
+                if i in word_freq:
+                    word_freq[i] += 1
+                else:
+                    word_freq[i] = 1
+
+            # Print frequency
+            for j in word_freq:
+                print(j, word_freq[j])
+
+            # Hash value
+            p = 53
+            mod = 2**64
+            hashvl = 0
+            pow= 1
+            for i in wordslist:
+                ascival= ord(i)
+                hashvl = (hashvl+ ascival* pow) % mod
+                pow= (pow * p) % mod
+            return hashvl
+
+        else:
+            print("Failed to fetch page")
+
+    except Exception as e:
+        print("Error occurred:", e)
+
+
+if __name__ == "__main__":
+    main()
